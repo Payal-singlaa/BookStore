09b31e4c7690ab88c385862e7f901cf31789398d
Mayank Singla <mayanksingla13@gmail.com>
2024-11-18 23:23:58 +0530

    first commit

---
+++ frontend/src/home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Freebook from '../components/Freebook'
+import Navbar from '../components/Navbar'
+import Banner from '../components/Banner'
+import Footer from '../components/Footer'
+
+function Home() {
+  return (
+    <>
+        <Navbar />
+        <Banner/>
+        <Freebook/>
+        
+        <Footer/>
+    </>
+  )
+}
+
+export default Home
